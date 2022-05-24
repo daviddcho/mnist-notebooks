9ec007950589459f715a10bb8ea3f631119ab733
gpuslave38 <amazingdcho@gmail.com>
2022-05-24 13:03:59 -0400

    conv model

--- gan.py
+++ gan.py
@@ -5,6 +5,7 @@ import numpy as np
 import torch
 import torch.nn as nn
 from torch import optim, tensor
+import torch.nn.functional as F
 from tqdm import trange
 import matplotlib.pyplot as plt
 
@@ -13,6 +14,9 @@ parse = lambda file: np.frombuffer(gzip.open(file).read(), dtype=np.uint8).copy(
 X_train = parse("data/train-images-idx3-ubyte.gz")[0x10:].reshape((-1, 28, 28))
 Y_train = parse("data/train-labels-idx1-ubyte.gz")[8:]
 
+from model import Generator, Discriminator
+
+"""
 class Generator(nn.Module):
   def __init__(self):
     super(Generator, self).__init__()
@@ -33,7 +37,6 @@ class Generator(nn.Module):
 class Discriminator(nn.Module):
   def __init__(self):
     super(Discriminator, self).__init__()
-    """
     self.layers = nn.Sequential( 
       nn.Linear(784, 1024),
       nn.LeakyReLU(0.2, inplace=False),
@@ -49,7 +52,6 @@ class Discriminator(nn.Module):
       #nn.Softmax(dim=1)
       #nn.Sigmoid()
     )
-    """
     self.l1 = nn.Linear(784, 1024)
     self.l2 = nn.Linear(1024, 512)
     self.l3 = nn.Linear(512, 256)
@@ -75,14 +77,14 @@ class Discriminator(nn.Module):
     #print("logsoftmax", x[0])
     return x
     #return self.layers(x)#.reshape(-1)
-
+"""
 
 if __name__ == "__main__":
   BS = 512
   k = 1 
   epochs = 50 #300
   n_steps = int(X_train.shape[0]/BS)
-  print(n_steps)
+  z_dim = 100
 
   device = 'cuda' if torch.cuda.is_available() else 'cpu'
   print("device: ", device)
@@ -95,24 +97,30 @@ if __name__ == "__main__":
 
   optim_g = optim.Adam(generator.parameters(), lr=0.0002, betas=(0.5, 0.999))
   optim_d = optim.Adam(discriminator.parameters(), lr=0.0002, betas=(0.5, 0.999))
-  ds_noise = tensor(np.random.randn(64, 128).astype(np.float32), requires_grad=False).to(device)
+  #ds_noise = tensor(np.random.randn(64, z_dim).astype(np.float32), requires_grad=False).to(device)
+  ds_noise = tensor(np.random.randn(64, z_dim, 1, 1).astype(np.float32), requires_grad=False).to(device)
 
   def generator_batch():
     samp = np.random.randint(0, X_train.shape[0], size=(BS))
     image = X_train[samp].reshape(-1, 28*28).astype(np.float32)/255.
     image = (image - 0.5)/0.5
+    image = image.reshape(-1, 1, 28, 28)
     return tensor(image).to(device)
 
   def real_label(bs):
+    """
     y = np.zeros((bs,2), dtype=np.float32)
     y[:,1] = -2
-    #print("real label", y.shape, y)
+    """
+    y = np.ones((bs,), dtype=np.float32)
     return tensor(y).to(device)
 
   def fake_label(bs):
+    """
     y = np.zeros((bs,2), dtype=np.float32)
     y[:,0] = -2 #-1.98789 
-    #print("fake label", y.shape, y)
+    """
+    y = np.zeros((bs,), dtype=np.float32)
     return tensor(y).to(device)
 
   def train_discriminator(optimizer, real_data, fake_data):
@@ -122,13 +130,19 @@ if __name__ == "__main__":
     optimizer.zero_grad()
 
     #print("real data", real_data.shape, real_data)
-    real_out = discriminator(real_data)
+    real_out = discriminator(real_data).view(-1)
     #print("discrim real out", real_out.shape, real_out)
-    real_loss = (real_out * real_labels).mean()
+    #real_loss = (real_out * real_labels).mean()
+    real_loss = F.binary_cross_entropy(real_out, real_labels)
     #print("real_loss", real_loss)
 
+    print("fake data", fake_data.shape)
     fake_out = discriminator(fake_data)
-    fake_loss = (fake_out * fake_labels).mean()
+    print(fake_out.shape)
+    fake_out = fake_out.view(-1)
+    print(fake_out.shape)
+    #fake_loss = (fake_out * fake_labels).mean()
+    fake_loss = F.binary_cross_entropy(fake_out, fake_labels)
     #print("fake_loss", fake_loss)
     print("total", real_loss.item() + fake_loss.item())
 
@@ -142,7 +156,8 @@ if __name__ == "__main__":
     y = real_label(BS) 
     optimizer.zero_grad()
     out = discriminator(fake_data)
-    loss = (out * y).mean() 
+    #loss = (out * y).mean() 
+    loss = F.binary_loss_entropy(out, y)
     loss.backward()
     optimizer.step()
     return loss.item()
@@ -155,12 +170,16 @@ if __name__ == "__main__":
       for step in range(k):
         # train discriminator
         real_data = generator_batch()
-        noise = tensor(np.random.rand(BS, 128)).float().to(device)
+        print(real_data.shape)
+        #noise = tensor(np.random.rand(BS, z_dim)).float().to(device)
+        noise = tensor(np.random.rand(BS, z_dim, 1, 1)).float().to(device)
+
         fake_data = generator(noise).detach()
         loss_d_step = train_discriminator(optim_d, real_data, fake_data)
         loss_d += loss_d_step
       # train generator
-      noise = tensor(np.random.rand(BS, 128)).float().to(device)
+      #noise = tensor(np.random.rand(BS, z_dim)).float().to(device)
+      noise = tensor(np.random.rand(BS, z_dim, 1, 1)).float().to(device)
       fake_data = generator(noise).detach()
       loss_g_step = train_generator(optim_g, fake_data)
       loss_g += loss_g_step
@@ -170,6 +189,7 @@ if __name__ == "__main__":
   
     fake_images = generator(ds_noise).detach()
     fake_images = ((fake_images.reshape(-1, 28, 28)+1)/2).cpu().numpy()
+    print(fake_images.shape)
     fake_images = np.concatenate(fake_images.reshape((8, 8*28, 28)), axis=1)
     plt.figure(figsize=(8,8))
     plt.imshow(fake_images)
---
+++ model.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python3
+import torch 
+from torch import optim, nn
+import torch.nn.functional as F
+
+
+z_dim = 100
+
+class Generator(nn.Module):
+  def __init__(self):
+    super(Generator, self).__init__()
+    self.layers = nn.Sequential(
+      nn.ConvTranspose2d(z_dim, 28*8, 4, 1, 0, bias=False),
+      nn.BatchNorm2d(28*8), 
+      nn.ReLU(True),
+      nn.ConvTranspose2d(28*8, 28*4, 4, 2, 0, bias=False),
+      nn.BatchNorm2d(28*4), 
+      nn.ReLU(True),
+      nn.ConvTranspose2d(28*4, 28*2, 4, 2, 1, bias=False),
+      nn.BatchNorm2d(28*2), 
+      nn.ReLU(True),
+      nn.ConvTranspose2d(28*2, 28, 4, 2, 1, bias=False),
+      nn.BatchNorm2d(28), 
+      nn.ReLU(True),
+      nn.ConvTranspose2d(28, 1, 4, 2, 1, bias=False),
+      nn.Tanh() # [-1, 1]
+      # n_channels x 28 x 28
+    )
+
+  def forward(self, x):
+      return self.layers(x)
+
+class Discriminator(nn.Module):
+  def __init__(self):
+    super(Discriminator, self).__init__()
+    self.layers = nn.Sequential(
+      # n_channels x 28 x 28
+      nn.Conv2d(1, 28, 4, 2, 1, bias=False),
+      nn.LeakyReLU(0.2, inplace=True),
+      nn.Conv2d(28, 28*2, 4, 2, 1, bias=False),
+      nn.BatchNorm2d(28*2),
+      nn.LeakyReLU(0.2, inplace=True),
+      nn.Conv2d(28*2, 28*4, 4, 2, 1, bias=False),
+      nn.BatchNorm2d(28*4),
+      nn.LeakyReLU(0.2, inplace=True),
+      nn.Conv2d(28*4, 28*8, 4, 2, 1, bias=False),
+      nn.BatchNorm2d(28*8),
+      nn.LeakyReLU(0.2, inplace=True),
+      nn.Conv2d(28*8, 1, 1, 1, 0, bias=False),
+      nn.Sigmoid()
+    )
+
+  def forward(self, x):
+      return self.layers(x)
+
+if __name__ == "__main__":
+  device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+  gen = Generator().to(device)
+  dis = Discriminator().to(device)
+
+  optim_g = optim.Adam(gen.parameters(), lr=0.0002, betas=(0.5, 0.999))
+  optim_d = optim.Adam(dis.parameters(), lr=0.0002, betas=(0.5, 0.999))
+
+  batch_size = 128
+  epochs = 5
+  n_steps = int(X_train.size/batch_size)
+
+  for epoch in num_epoch:
+    for i in n_steps:
+      pass
