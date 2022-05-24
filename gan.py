27b982e1f44b2f64bb73cf46cd2c0fd93cb75df1
gpuslave38 <amazingdcho@gmail.com>
2022-05-24 18:22:48 -0400

    full pivot

--- gan.py
+++ gan.py
@@ -16,69 +16,6 @@ Y_train = parse("data/train-labels-idx1-ubyte.gz")[8:]
 
 from model import Generator, Discriminator
 
-"""
-class Generator(nn.Module):
-  def __init__(self):
-    super(Generator, self).__init__()
-    self.layers = nn.Sequential(
-      nn.Linear(128, 256),
-      nn.LeakyReLU(0.2),
-      nn.Linear(256, 512),
-      nn.LeakyReLU(0.2),
-      nn.Linear(512, 1024),
-      nn.LeakyReLU(0.2),
-      nn.Linear(1024, 784),
-      nn.Tanh()
-    )
-  
-  def forward(self, x):
-    return self.layers(x)
-
-class Discriminator(nn.Module):
-  def __init__(self):
-    super(Discriminator, self).__init__()
-    self.layers = nn.Sequential( 
-      nn.Linear(784, 1024),
-      nn.LeakyReLU(0.2, inplace=False),
-      nn.Dropout(0.3),
-      nn.Linear(1024, 512),
-      nn.LeakyReLU(0.2, inplace=False),
-      nn.Dropout(0.3),
-      nn.Linear(512, 256),
-      nn.LeakyReLU(0.2, inplace=False),
-      nn.Dropout(0.3),
-      nn.Linear(256, 2),
-      nn.LogSoftmax(dim=1)
-      #nn.Softmax(dim=1)
-      #nn.Sigmoid()
-    )
-    self.l1 = nn.Linear(784, 1024)
-    self.l2 = nn.Linear(1024, 512)
-    self.l3 = nn.Linear(512, 256)
-    self.l4 = nn.Linear(256, 2)
-    self.dropout = nn.Dropout(0.3)
-    self.leakyrelu = nn.LeakyReLU(0.2)
-    self.logsoftmax = nn.LogSoftmax(dim=1)
-
-  def forward(self, x, train=True):
-    #print("input", x[0])
-    x = self.leakyrelu(self.l1(x))
-    if train:
-      x = self.dropout(x)
-    x = self.leakyrelu(self.l2(x))
-    if train: 
-      x = self.dropout(x)
-    x = self.leakyrelu(self.l3(x))
-    if train:
-      x = self.dropout(x)
-    x = self.l4(x)
-    #print("before logsoftmax", x[0])
-    x = self.logsoftmax(x)
-    #print("logsoftmax", x[0])
-    return x
-    #return self.layers(x)#.reshape(-1)
-"""
-
 if __name__ == "__main__":
   BS = 128
   k = 1 
@@ -102,58 +39,32 @@ if __name__ == "__main__":
 
   def generator_batch():
     samp = np.random.randint(0, X_train.shape[0], size=(BS))
-    image = X_train[samp].reshape(-1, 28*28).astype(np.float32)/255.
+    #image = X_train[samp].reshape(-1, 28*28).astype(np.float32)/255.
+    image = X_train[samp].astype(np.float32)/255.
     image = (image - 0.5)/0.5
     image = image.reshape(-1, 1, 28, 28)
     return tensor(image).to(device)
 
-  def real_label(bs):
-    """
-    y = np.zeros((bs,2), dtype=np.float32)
-    y[:,1] = -2
-    """
-    y = np.ones((bs,), dtype=np.float32)
-    return tensor(y).to(device)
-
-  def fake_label(bs):
-    """
-    y = np.zeros((bs,2), dtype=np.float32)
-    y[:,0] = -2 #-1.98789 
-    """
-    y = np.zeros((bs,), dtype=np.float32)
-    return tensor(y).to(device)
-
   def train_discriminator(optimizer, real_data, fake_data):
-    real_labels = real_label(BS)
-    fake_labels = fake_label(BS)
-
     optimizer.zero_grad()
 
-    #print("real data", real_data.shape, real_data)
-    real_out = discriminator(real_data).view(-1)
-    #print("discrim real out", real_out.shape, real_out)
-    #real_loss = (real_out * real_labels).mean()
-    real_loss = F.binary_cross_entropy(real_out, real_labels)
-    #print("real_loss", real_loss)
+    out = discriminator(real_data)
+    y = torch.ones(BS, dtype=torch.float32).to(device)
+    real_loss = F.binary_cross_entropy(out, y)
 
-    #fake_data = torch.rand(BS, 1, 28, 28).float().cuda()
-    fake_out = discriminator(fake_data).view(-1)
-    #fake_loss = (fake_out * fake_labels).mean()
-    fake_loss = F.binary_cross_entropy(fake_out, fake_labels)
-    #print("fake_loss", fake_loss)
-    #print("total", real_loss.item() + fake_loss.item())
+    out = discriminator(fake_data)
+    y = torch.zeros(BS, dtype=torch.float32).to(device)
+    fake_loss = F.binary_cross_entropy(out, y)
 
     real_loss.backward() 
     fake_loss.backward()
     optimizer.step()
-    #print(real_loss.item(), fake_loss.item())
     return real_loss.item() + fake_loss.item()
 
   def train_generator(optimizer, fake_data):
-    y = real_label(BS) 
+    y = torch.ones(BS, dtype=torch.float32).to(device)
     optimizer.zero_grad()
-    out = discriminator(fake_data).view(-1)
-    #loss = (out * y).mean() 
+    out = discriminator(fake_data)
     loss = F.binary_cross_entropy(out, y)
     loss.backward()
     optimizer.step()
@@ -167,15 +78,13 @@ if __name__ == "__main__":
       for step in range(k):
         # train discriminator
         real_data = generator_batch()
-        #print(real_data.shape)
-        #noise = tensor(np.random.rand(BS, z_dim)).float().to(device)
         noise = tensor(np.random.rand(BS, z_dim, 1, 1)).float().to(device)
 
         fake_data = generator(noise).detach()
         loss_d_step = train_discriminator(optim_d, real_data, fake_data)
         loss_d += loss_d_step
+
       # train generator
-      #noise = tensor(np.random.rand(BS, z_dim)).float().to(device)
       noise = tensor(np.random.rand(BS, z_dim, 1, 1)).float().to(device)
       fake_data = generator(noise).detach()
       loss_g_step = train_generator(optim_g, fake_data)
@@ -183,10 +92,11 @@ if __name__ == "__main__":
 
     losses_g.append(loss_g)
     losses_d.append(loss_d)
-  
+
+    # generate from fixed noise  
     fake_images = generator(ds_noise).detach()
-    fake_images = ((fake_images.reshape(-1, 28, 28)+1)/2).cpu().numpy()
-    print(fake_images.shape)
+    #fake_images = ((fake_images.reshape(-1, 28, 28)+1)/2).cpu().numpy()
+    fake_images = fake_images.reshape(-1, 28, 28).cpu().numpy()
     fake_images = np.concatenate(fake_images.reshape((8, 8*28, 28)), axis=1)
     plt.figure(figsize=(8,8))
     plt.imshow(fake_images)
--- model.py
+++ model.py
@@ -48,7 +48,7 @@ class Discriminator(nn.Module):
     )
 
   def forward(self, x):
-      return self.layers(x)
+      return self.layers(x).view(-1)
 
 if __name__ == "__main__":
   device = 'cuda' if torch.cuda.is_available() else 'cpu'
