cbf4d867847942a7e5344dbbfde8bc1ec77d398a
gpuslave38 <amazingdcho@gmail.com>
2022-05-24 18:45:53 -0400

    smaller discrim model

--- model.py
+++ model.py
@@ -25,7 +25,7 @@ class Generator(nn.Module):
     )
 
   def forward(self, x):
-      return self.layers(x)
+    return self.layers(x)
 
 class Discriminator(nn.Module):
   def __init__(self):
@@ -34,21 +34,28 @@ class Discriminator(nn.Module):
       # n_channels x 28 x 28
       nn.Conv2d(1, 28, 4, 2, 1, bias=False),
       nn.LeakyReLU(0.2, inplace=True),
+
       nn.Conv2d(28, 28*2, 4, 2, 1, bias=False),
       nn.BatchNorm2d(28*2),
       nn.LeakyReLU(0.2, inplace=True),
-      nn.Conv2d(28*2, 28*4, 4, 2, 1, bias=False),
+
+      nn.Conv2d(28*2, 28*4, 3, 2, 1, bias=False),
       nn.BatchNorm2d(28*4),
       nn.LeakyReLU(0.2, inplace=True),
-      nn.Conv2d(28*4, 28*8, 4, 2, 1, bias=False),
-      nn.BatchNorm2d(28*8),
-      nn.LeakyReLU(0.2, inplace=True),
-      nn.Conv2d(28*8, 1, 1, 1, 0, bias=False),
-      nn.Sigmoid()
+
+      nn.Conv2d(28*4, 1, 4, 1, 0, bias=False),
+      nn.Sigmoid(),
     )
 
   def forward(self, x):
-      return self.layers(x).view(-1)
+    return self.layers(x).view(-1)
+
+class PrintLayer(nn.Module):
+  def __init__(self):
+    super(PrintLayer, self).__init__()
+  def forward(self, x):
+    print(f"within layers, {x.shape}")
+    return x
 
 if __name__ == "__main__":
   device = 'cuda' if torch.cuda.is_available() else 'cpu'
