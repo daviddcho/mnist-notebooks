f3ef89791642603e52f20faf3757a4b9da4a1ee9
gpuslave38 <amazingdcho@gmail.com>
2022-05-26 13:13:23 -0400

    seems pretty solid

--- gan.py
+++ gan.py
@@ -15,8 +15,8 @@ parse = lambda file: np.frombuffer(gzip.open(file).read(), dtype=np.uint8).copy(
 X_train = parse("data/train-images-idx3-ubyte.gz")[0x10:].reshape((-1, 28, 28))
 
 if __name__ == "__main__":
-  batch_size = 256
-  n_epochs = 20
+  batch_size = 128
+  n_epochs = 30
   n_batches = int(X_train.shape[0]/batch_size)
   z_dim = 100
 
@@ -29,12 +29,9 @@ if __name__ == "__main__":
   generator = Generator().to(device)
   discriminator = Discriminator().to(device)
 
-  loss_function = nn.BCELoss()
-
-  optim_g = optim.Adam(generator.parameters(), lr=0.0002)#, betas=(0.5, 0.999))
-  optim_d = optim.Adam(discriminator.parameters(), lr=0.0002)#, betas=(0.5, 0.999))
-  ds_noise = tensor(np.random.randn(64, z_dim, 1, 1).astype(np.float32), requires_grad=False).to(device)
-  #ds_noise = torch.randn(64, 100, 1, 1, device=device)
+  optim_g = optim.Adam(generator.parameters(), lr=0.0002)
+  optim_d = optim.Adam(discriminator.parameters(), lr=0.0002)
+  ds_noise = torch.randn(64, 100, 1, 1, device=device)
 
   def generate_batch():
     samp = np.random.randint(0, X_train.shape[0], size=(batch_size))
@@ -67,59 +64,34 @@ if __name__ == "__main__":
     optim_g.step()
     return loss
 
-  def save_fake_images(generator, ds_noise, epoch, path="out"):
+  def save_fake_images(generator, ds_noise, epoch, timestamp, path="out"):
     fake_images = generator(ds_noise)
     fake_images = fake_images.reshape(-1, 28, 28).cpu().detach().numpy()
     fake_images = np.concatenate(fake_images.reshape((8, 28*8, 28)), axis=1)
     plt.figure(figsize=(8,8))
     plt.imshow(fake_images)
-    plt.savefig(f"{path}/images_{epoch}.png")
+    plt.savefig(f"{path}/images_{timestamp}_{epoch}.png")
     plt.close()
+
+  import time
+  timestamp = int(time.time())
 
   losses_g, losses_d = [], []
   for epoch in trange(n_epochs):
     for i in (t := trange(n_batches)):
       # train discriminator
-      """
-      optim_d.zero_grad()
-      real_data = generate_batch()
-      y = torch.ones(batch_size).float().to(device)
-      out = discriminator(real_data)
-      real_loss = loss_function(out, y)
-      real_loss.backward() 
-
-      noise = torch.randn(batch_size, z_dim, 1, 1).to(device)
-      fake_data = generator(noise)
-      y = torch.zeros(batch_size).float().to(device)
-      out = discriminator(fake_data.detach())
-      fake_loss = loss_function(out, y)
-      fake_loss.backward()
-      loss_d = real_loss + fake_loss
-      optim_d.step()
-      """
       real_data = generate_batch()
       noise = torch.randn(batch_size, z_dim, 1, 1).to(device)
       fake_data = generator(noise)
       loss_d = train_discriminator(real_data, fake_data)
 
       # train generator
-      """
-      optim_g.zero_grad()
-      y = torch.ones(batch_size).float().to(device)
-      out = discriminator(fake_data)
-      loss_g = loss_function(out, y)
-      loss_g.backward()
-      optim_g.step()
-      """
-      noise = torch.randn(batch_size, z_dim, 1, 1).to(device)
-      fake_data = generator(noise)
       loss_g = train_generator(fake_data)
 
       t.set_description("loss_g %.4f loss_d %.4f" % (loss_g.item(), loss_d.item()))
-     
       losses_g.append(loss_g.item())
       losses_d.append(loss_d.item())
-    save_fake_images(generator, ds_noise, epoch)
+    save_fake_images(generator, ds_noise, epoch, timestamp)
     
   plt.plot(losses_g, label="generator loss")
   plt.plot(losses_d, label="discriminator loss")
--- model.py
+++ model.py
@@ -3,11 +3,8 @@ import torch
 from torch import optim, nn
 import torch.nn.functional as F
 
-
-z_dim = 100
-
 class Generator(nn.Module):
-  def __init__(self):
+  def __init__(self, z_dim=100):
     super(Generator, self).__init__()
     self.layers = nn.Sequential(
       nn.ConvTranspose2d(z_dim, 28*4, 4, 1, 0, bias=False),
@@ -20,8 +17,8 @@ class Generator(nn.Module):
       nn.BatchNorm2d(28), 
       nn.ReLU(True),
       nn.ConvTranspose2d(28, 1, 4, 2, 1, bias=False),
-      nn.Tanh(), # [-1, 1]
-      # n_channels x 28 x 28
+      nn.Tanh(),
+      # n_ch x 28 x 28
     )
 
   def forward(self, x):
@@ -59,17 +56,5 @@ class PrintLayer(nn.Module):
 
 if __name__ == "__main__":
   device = 'cuda' if torch.cuda.is_available() else 'cpu'
-
   gen = Generator().to(device)
   dis = Discriminator().to(device)
-
-  optim_g = optim.Adam(gen.parameters(), lr=0.0002, betas=(0.5, 0.999))
-  optim_d = optim.Adam(dis.parameters(), lr=0.0002, betas=(0.5, 0.999))
-
-  batch_size = 128
-  epochs = 5
-  n_steps = int(X_train.size/batch_size)
-
-  for epoch in num_epoch:
-    for i in n_steps:
-      pass
