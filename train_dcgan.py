dcb4a49689b84ed192f818dbdb46da811883fd03
gpuslave38 <amazingdcho@gmail.com>
2022-05-26 19:12:54 -0400

    bigger data

--- train_dcgan.py
+++ train_dcgan.py
@@ -1,18 +1,23 @@
 #!/usr/bin/env python3
 import os
 import gzip
+import time
 import numpy as np 
 import torch
 import torch.nn as nn
 from torch import optim, tensor
 import torch.nn.functional as F
-from tqdm import trange
 import matplotlib.pyplot as plt
+from tqdm import trange
 from dcgan import Generator, Discriminator
 
-# load data
-parse = lambda file: np.frombuffer(gzip.open(file).read(), dtype=np.uint8).copy()
-X_train = parse("data/train-images-idx3-ubyte.gz")[0x10:].reshape((-1, 28, 28))
+def load_data():
+  parse = lambda file: np.frombuffer(gzip.open(file).read(), dtype=np.uint8).copy()
+  X_train = parse("data/train-images-idx3-ubyte.gz")[0x10:].reshape((-1, 28, 28))
+  X_test = parse("data/t10k-images-idx3-ubyte.gz")[0x10:].reshape((-1, 28, 28))
+  X_train = np.concatenate((X_train, X_test), axis=0)
+  return X_train
+X_train = load_data()
 
 batch_size = 128
 n_epochs = 30
@@ -22,15 +27,13 @@ z_dim = 100
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 print("device: ", device)
 
-out_dir = "out"
-os.makedirs(out_dir, exist_ok=True)
 
 generator = Generator().to(device)
 discriminator = Discriminator().to(device)
 
 optim_g = optim.Adam(generator.parameters(), lr=0.0002)
 optim_d = optim.Adam(discriminator.parameters(), lr=0.0002)
-ds_noise = torch.randn(64, 100, 1, 1, device=device)
+fixed_noise = torch.randn(64, 100, 1, 1, device=device)
 
 def generate_batch():
   samp = np.random.randint(0, X_train.shape[0], size=(batch_size))
@@ -63,18 +66,18 @@ def train_generator(fake_data):
   optim_g.step()
   return loss
 
-def save_fake_images(generator, ds_noise, epoch, timestamp, path="out"):
-  fake_images = generator(ds_noise)
+out_dir = "out"
+os.makedirs(out_dir, exist_ok=True)
+def save_fake_images(generator, fixed_noise, epoch, timestamp):
+  fake_images = generator(fixed_noise)
   fake_images = fake_images.reshape(-1, 28, 28).cpu().detach().numpy()
   fake_images = np.concatenate(fake_images.reshape((8, 28*8, 28)), axis=1)
   plt.figure(figsize=(8,8))
   plt.imshow(fake_images)
-  plt.savefig(f"{path}/images_{timestamp}_{epoch}.png")
+  plt.savefig(f"{out_dir}/images_{timestamp}_{epoch}.png")
   plt.close()
 
-import time
 timestamp = int(time.time())
-
 losses_g, losses_d = [], []
 for epoch in trange(n_epochs):
   for i in (t := trange(n_batches)):
@@ -90,10 +93,12 @@ for epoch in trange(n_epochs):
     t.set_description("loss_g %.4f loss_d %.4f" % (loss_g.item(), loss_d.item()))
     losses_g.append(loss_g.item())
     losses_d.append(loss_d.item())
-  save_fake_images(generator, ds_noise, epoch, timestamp)
+  save_fake_images(generator, fixed_noise, epoch, timestamp)
   
+"""
 plt.plot(losses_g, label="generator loss")
 plt.plot(losses_d, label="discriminator loss")
 plt.legend()
 plt.savefig("loss.png")
 plt.close()
+"""
