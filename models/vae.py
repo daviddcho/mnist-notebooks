beb04902e4b87b02ffa03f2df07461417e9d6d06
gpuslave38 <amazingdcho@gmail.com>
2022-06-05 16:02:18 -0400

    vae sucks

--- models/vae.py
+++ models/vae.py
@@ -4,26 +4,23 @@ import torch.nn as nn
 #from dcgan import PrintLayer
 
 class VAE(nn.Module):
-  def __init__(self, zdim=20):
+  def __init__(self, zdim=4):
     super(VAE, self).__init__()
     self.conv = nn.Sequential(
-      #PrintLayer(),
       nn.Conv2d(1, 64, 2, 2),
       nn.BatchNorm2d(64), 
       nn.ReLU(),
-      #PrintLayer(),
       nn.Conv2d(64, 128, 2, 2),
       nn.BatchNorm2d(128),
       nn.ReLU(),
-      #PrintLayer(),
       nn.Conv2d(128, 256, 4, 2),
       nn.BatchNorm2d(256),
       nn.ReLU(),
-      #PrintLayer(),
     )
     self.fc1 = nn.Linear(1024, zdim)
+    self.fc2 = nn.Linear(1024, zdim)
 
-    self.fc2 = nn.Sequential(
+    self.fc3 = nn.Sequential(
       nn.Linear(zdim, 1024),
       nn.BatchNorm1d(1024),
       nn.ReLU(),
@@ -42,18 +39,23 @@ class VAE(nn.Module):
   def encoder(self, x):
     x = self.conv(x)
     x = x.reshape(x.shape[0], -1)
-    x = self.fc1(x)
-    return x
+    return self.fc1(x), self.fc2(x)
+
+  def reparameterize(self, mu, logvar):
+    std = torch.exp(logvar*0.5)
+    eps = torch.randn_like(std)
+    return mu + eps*std
  
   def decoder(self, x):
-    x = self.fc2(x)
+    x = self.fc3(x)
     x = x.reshape(x.shape[0], -1, 1, 1)
     x = self.conv_transpose(x)
     return x
 
   def forward(self, x):
-    z = self.encoder(x)
-    return self.decoder(z)
+    mu, logvar = self.encoder(x)
+    z = self.reparameterize(mu, logvar)
+    return self.decoder(z), mu, logvar
 
 if __name__ == "__main__":
   vae = VAE()
