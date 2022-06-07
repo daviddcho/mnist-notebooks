768b6de9525b814c871bb7c832adc156ac3c4716
gpuslave38 <amazingdcho@gmail.com>
2022-06-07 17:41:17 -0400

    vae needs normalized data, good loss now

--- train_vae.py
+++ train_vae.py
@@ -9,17 +9,19 @@ import gzip
 import time
 
 parse = lambda file: np.frombuffer(gzip.open(file).read(), dtype=np.uint8).copy()
-X_train = parse("data/train-images-idx3-ubyte.gz")[0x10:].reshape((-1, 28, 28))
-X_test = parse("data/t10k-images-idx3-ubyte.gz")[0x10:].reshape((-1, 28, 28))
+X_train = parse("data/train-images-idx3-ubyte.gz")[0x10:].reshape((-1, 28, 28))/255.
+X_test = parse("data/t10k-images-idx3-ubyte.gz")[0x10:].reshape((-1, 28, 28))/255.
 
-batch_size = 128
+batch_size = 64
 n_epochs = 100  
 n_batches = len(X_train)//batch_size
 
 from models.vae import VAE
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
-model = VAE(zdim=20).to(device)
-optimizer = torch.optim.Adam(model.parameters())
+model = VAE(zdim=128).to(device)
+import apex
+optimizer = apex.optimizers.FusedAdam(model.parameters(), lr=0.001)
+#optimizer = torch.optim.Adam(model.parameters())
 def loss_function(x, target, mu, logvar):
   mse_loss = F.mse_loss(x, target, reduction='sum')
   #mse_loss = F.binary_cross_entropy(x, target, reduction='sum')
@@ -28,7 +30,7 @@ def loss_function(x, target, mu, logvar):
 
 timestamp = int(time.time())
 for epoch in trange(n_epochs):
-  for i in (t := trange(n_batches)):
+  for n in (t := trange(n_batches)):
     samp = np.random.randint(0, X_train.shape[0], size=(batch_size))
     X = torch.tensor(X_train[samp]).reshape(-1, 1, 28, 28).float().to(device)
     optimizer.zero_grad()
@@ -38,10 +40,10 @@ for epoch in trange(n_epochs):
     optimizer.step()
     t.set_description(f"loss {loss:.2f}")
 
-  samp = np.random.randint(0, X_test.shape[0], size=(batch_size//2))
-  X = torch.tensor(X_test[samp]).reshape(-1, 1, 28, 28).float().to(device)
-  images, _, _ = model(X)
-  images = images.cpu().detach().numpy().reshape(-1, 28, 28)
-  images = np.concatenate(images.reshape(8, 28*8, 28), axis=1)
-  plt.imshow(images)
-  plt.savefig(f"out/images_{timestamp}_{epoch}")
+  with torch.no_grad():
+    X = torch.randn(64, 1, 28, 28).float().to(device)
+    images, _, _ = model(X)
+    images = images.cpu().detach().numpy().reshape(-1, 28, 28)
+    images = np.concatenate(images.reshape(8, 28*8, 28), axis=1)
+    plt.imshow(images, cmap='gray')
+    plt.savefig(f"out/images_{timestamp}_{epoch}")
